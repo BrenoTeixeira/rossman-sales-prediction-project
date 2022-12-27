312585c10c392ca3420030982983c6f81dee2dbd
BrenoTeixeira <teixcbreno@gmail.com>
2022-12-27 00:31:26 -0300

    bot - link api

--- rossmann_bot/bot.py
+++ rossmann_bot/bot.py
@@ -58,7 +58,7 @@ def predict(update, context):
     data = load_database(update, context)
     chat_id = update.effective_chat.id
     # Chamada na API
-    url = 'https://https://rossmann-api.onrender.com/rossman/predict'
+    url = 'https://brenoteixeira-rossmann-api-handler-73wgwz.streamlit.app/'
     header = {'Content-type': 'application/json'}
 
     if data != 'error':
